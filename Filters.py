6c7660d909cafe3ac3c8ca69789303de339ae07c
bysju <bysju@hotmail.com>
2009-03-19 13:39:47 +0900

    implement Filters.py

--- Filters.py
+++ Filters.py
@@ -18,9 +18,9 @@ class SrcFileFilter(pipesAndFilters.ThreadFilters):
         #read file
         lines = self.__fine.readlines()
         for line in lines:
-            self.__sinkPipe.push(line)  #push file data
+            self.__sinkPipe.put(line)  #push file data
 
-        self.__sinkPipe.push( queuePipe.TERMINATE)  #terminate job
+        self.__sinkPipe.put( queuePipe.TERMINATE)  #terminate job
 
 class SinkFileFilter(pipesAndFilters.ThreadFilters):
     def __init__(self, sinkFile, sourcePipe  ):
@@ -28,7 +28,7 @@ class SinkFileFilter(pipesAndFilters.ThreadFilters):
         self.__file = open(sinkFile, 'w')
         assert(None != sourcePipe)
     def run(self):
-        data = self.__sourcePipe.pull()
+        data = self.__sourcePipe.get()
         if isinstance( data, queuePipe.TERMINATE ) :
             return      #finish job
 
@@ -48,18 +48,62 @@ class DelTagFilter(pipesAndFilters.ThreadFilters):
 
     def run(self):
         while True :
-            self.__data = self.__sourcePipe.pull() # read data
+            self.__data = self.__sourcePipe.get() # read data
             if isinstance( data, queuePipe.TERMINATE ) :
                 if STATE_ENUM.FIND_STARTTAG != self.__state :
                     error.errDesc = "TAG NOT MATCH"
                     error.errCode = ERROR_CODE.TAG_NOT_MATCH
-                self.__sinkPipe.push(self.__data)
+                self.__sinkPipe.put(self.__data)
                 return          #finish job
 
+            self.__startPos = 0                   # not find
+            self.__curTag = None
             __process()                           # process data
 
-            self.__sinkPipe.push(self.__data)       # send data
+            self.__sinkPipe.put(self.__data)       # send data
 
     def __process():
-        while True : 
+        while True :
+            if STATE_ENUM.FIND_STARTTAG == self.__state :
+                
+                findPos = -1 # not found
+                
+                for startTag in self.__tagList:
+                    pos = self.__data.find( startTag, self.__startPos )
+                    
+                    if -1 != pos :  # find
+                        if -1 == findPos:      #first find
+                            findPos = pos
+                            self.__curTag = startTag
+                        else:
+                            if findPos > pos:
+                                findPos = pos
+                                self.__curTag = startTag
+
+                if -1 == findPos :
+                    return              # if not found nothing to do
+                else:
+                    self.__startPos = findPos
+                    self.__state = STATE_ENUM.FIND_ENDTAG
+                    continue
+            elif STATE_ENUM.FIND_ENDTAG == self.__state:
+
+                endPos = -1 #not found
+
+                if None != self.__curTag :
+                    findPos = self.__startPos + len(self.__curTag)
+                else:
+                    findPos = self.__startPos
+
+                endPos = self.__data.find( self.__endTag , findPos )
+
+                if -1 != endPos : # find
+                    self.__data = self.__data[:self.__startPos] + self.__data[endPos:]
+                    self.__startPos = 0
+                    self.__state = STATE_ENUM.FIND_STARTTAG
+                    continue        #re find
+                else :              # not found
+                    self.__data = self.__data[:self.__startPos]
+                    return          # read next data
+                    
             
